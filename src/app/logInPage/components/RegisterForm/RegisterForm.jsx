09ebe1a57c896cdd09b982a234baff165d355611
Olha Zarichna <olya.zarichna@gmail.com>
2023-09-08 16:05:35 +0300

    change registerform

--- src/app/logInPage/components/RegisterForm/RegisterForm.jsx
+++ src/app/logInPage/components/RegisterForm/RegisterForm.jsx
@@ -11,10 +11,13 @@ const RegisterForm = () => {
   const [lastName, setLastName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [repeatPassword, setRepeatPassword] = useState('');
   const [showPassword, setShowPassword] = useState(false);
   const [emailError, setEmailError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
   const [emailOrPassError, setEmailOrPassError] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
+
   const onPasswordClick = () => {
     setShowPassword(!showPassword);
   };
@@ -35,31 +38,41 @@ const RegisterForm = () => {
       case 'password':
         setPassword(value);
         break;
+      case 'repeatPassword':
+        setRepeatPassword(value);
+        break;
       default:
         break;
     }
   };
+
   const handleSubmit = async e => {
     e.preventDefault();
-    if (password.trim() === '' || email.trim === '') {
+    if (!password.trim() || !email.trim()) {
       setEmailOrPassError(true);
-      setEmailError(false);
+      setPasswordError(false);
       return;
     }
-    setIsLoading(true);
+
+    const emailRegex = /^[A-Za-z0-9]+[A-Za-z0-9._]*@[A-Za-z]+\.[A-Za-z]{2,}$/;
+    if (!emailRegex.test(email)) {
+      setEmailOrPassError(false);
+      setEmailError(true);
+      return;
+    }
+    if (password !== repeatPassword) {
+      setEmailOrPassError(false);
+      setEmailError(false);
+      setPasswordError(true);
+      return;
+    }
     const user = {
       firstName: firstName || '',
       lastName: lastName || '',
       email,
       password,
     };
-    const emailRegex = /^[A-Za-z0-9]+[A-Za-z0-9._]*@[A-Za-z]+\.[A-Za-z]{2,}$/;
-    if (!emailRegex.test(email)) {
-      setEmailError(true);
-      setIsLoading(false);
-      setEmailOrPassError(false);
-      return;
-    }
+    setIsLoading(true);
     try {
       await register(user);
       router.push('/');
@@ -70,12 +83,14 @@ const RegisterForm = () => {
       setIsLoading(false);
       setEmailOrPassError(false);
     }
+    console.log('ggh');
   };
   const reset = () => {
     setFirstName('');
     setLastName('');
     setEmail('');
     setPassword('');
+    setRepeatPassword('');
   };
 
   return (
@@ -140,7 +155,35 @@ const RegisterForm = () => {
           </svg>
         </button>
       </div>
-      <button type="submit" className={styles.form__btn} disabled={isLoading}>
+      <div className={styles.passwordField}>
+        <InputField
+          htmlFor="repeatPassword"
+          label="Повторіть пароль"
+          type={showPassword ? 'text' : 'password'}
+          name="repeatPassword"
+          id="repeatPassword"
+          value={repeatPassword}
+          onChange={handleChange}
+          placeholder="aaaAA_1aaaa"
+        />
+        {passwordError && <p className={styles.error}>Паролі не співпадають</p>}
+        <button className={styles.form__btnIcon} onClick={onPasswordClick} type="button">
+          <svg
+            className={styles.form__icon}
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 488.85 488.85"
+          >
+            <path d="M244.425 98.725c-93.4 0-178.1 51.1-240.6 134.1-5.1 6.8-5.1 16.3 0 23.1 62.5 83.1 147.2 134.2 240.6 134.2s178.1-51.1 240.6-134.1c5.1-6.8 5.1-16.3 0-23.1-62.5-83.1-147.2-134.2-240.6-134.2zm6.7 248.3c-62 3.9-113.2-47.2-109.3-109.3 3.2-51.2 44.7-92.7 95.9-95.9 62-3.9 113.2 47.2 109.3 109.3-3.3 51.1-44.8 92.6-95.9 95.9zm-3.1-47.4c-33.4 2.1-61-25.4-58.8-58.8 1.7-27.6 24.1-49.9 51.7-51.7 33.4-2.1 61 25.4 58.8 58.8-1.8 27.7-24.2 50-51.7 51.7z" />
+          </svg>
+        </button>
+      </div>
+      <button
+        type="submit"
+        className={isLoading ? styles.form__btnDisabled : styles.form__btn}
+        disabled={isLoading}
+      >
         {isLoading ? 'Зачекайте...' : 'Зареєструватись'}
       </button>
     </form>
